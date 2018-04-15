c3c71f8adc8cea3d9eb3c44dae433b0771210a2c
B1kt0p <a.biktop@gmail.com>
2018-04-15 11:31:28 +0900

    First release.

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+
+124/5000
+The script prints users of [devman.org](https://devman.org), sending 
+tasks to check after 24:00 local time for the user.
+# Get started:
+
+An example of running a script in Linux, Python 3.5:
+```bash
+
+$ python3 seek_dev_nighters.py
+
+Sent tasks to check after 24:00 the following users:
+
+Vhjuvuvyuinopjolov
+avgcv.bcibcwqi
+niwqcp
+
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,58 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+import datetime
 
-def get_midnighters():
-    pass
+
+def get_load_data_api(url):
+    try:
+        response = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        return None
+    if response.ok:
+        return response.json()
+
+
+def get_number_of_pages(url):
+    page = 1
+    content = get_load_data_api(url.format(page))
+    if content:
+        return content['number_of_pages']
+
+
+def load_attempts(url, number_of_pages):
+    for page in range(1, number_of_pages+1):
+        content = get_load_data_api(url.format(page))
+        if content:
+            for attempt in content['records']:
+                yield attempt
+        else:
+            continue
+
+
+def get_midnighters(attempt):
+    attempt_date = datetime.datetime.fromtimestamp(
+        int(attempt['timestamp']),
+        pytz.timezone(attempt['timezone'])
+    )
+    midnite_hour = 0
+    morning_hour = 5
+    if midnite_hour <= attempt_date.hour <= morning_hour:
+        return attempt['username']
+
+
+def print_midnighters(midnighters):
+    if midnighters:
+        print('Sent tasks to check after 24:00 the following users:')
+        [print(midnighter) for midnighter in midnighters]
+        return True
+
 
 if __name__ == '__main__':
-  pass
+    url = 'http://devman.org/api/challenges/solution_attempts/?page={}'
+    number_of_pages = get_number_of_pages(url) or exit('Can not load content!')
+    midnighters = []
+    for attempt in load_attempts(url, number_of_pages):
+        if get_midnighters(attempt):
+            midnighters.append(get_midnighters(attempt))
+    midnighters = tuple(set(midnighters))
+    print_midnighters(midnighters) or exit('Midnighters not found!')
